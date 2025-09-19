0cdddb7a73295c847717f15aa36c6073c8cc6b2c
Dhruv Rathi <144977333+iamdhruvrathi@users.noreply.github.com>
2025-09-20 03:36:16 +0530

    Updated Header.jsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,37 +1,15 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
-  variable: "--font-poppins"
-});
-
-
-export const metadata: Metadata = {
-  title: "Evently",
-  description: "Evently is a platform for event managment.",
-  icons: {
-    icon: '/assets/images/logo.svg'
-  }
-};
+import Header from "@/components/shared/Header";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <main
-        className="flex-1"
-      >
-        {children}
-      </main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
